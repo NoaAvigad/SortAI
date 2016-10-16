e1b81923a3d232140af29bb9fc716d3a50ae9bc2
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-10-16 05:26:31 -0700

    sorting works

--- app/src/main/java/com/projects/dubhacks/sortai/Client.java
+++ app/src/main/java/com/projects/dubhacks/sortai/Client.java
@@ -32,22 +32,23 @@ public class Client {
     }
 
     // this method populate the predictionResults with default models
-    public Observable<List<ClarifaiOutput<Concept>>> predictWithGeneralModel(ClarifaiInput inputImage) {
+    public Observable<String> predictWithGeneralModel(ClarifaiInput inputImage) {
         Model<?> model = client.getDefaultModels().generalModel();
-        return predictWithModel(inputImage, model);
+        return predictWithModel(inputImage, model.id());
     }
+//
+//    // this method populate the predictionResults with default models
+//    public Observable<List<ClarifaiOutput<Concept>>> predictWithCustomModel(ClarifaiInput inputImage, String modelId) {
+//        Model<?> model = client.getModelByID(modelId).executeSync().get();
+//        return predictWithModel(inputImage, model);
+//    }
 
-    // this method populate the predictionResults with default models
-    public Observable<List<ClarifaiOutput<Concept>>> predictWithCustomModel(ClarifaiInput inputImage, String modelId) {
-        Model<?> model = client.getModelByID(modelId).executeSync().get();
-        return predictWithModel(inputImage, model);
-    }
-
-    private Observable<List<ClarifaiOutput<Concept>>> predictWithModel(final ClarifaiInput inputImage, final Model model){
+    public Observable<String> predictWithModel(final ClarifaiInput inputImage, final String modelId){
         try {
-            return Observable.create(new Observable.OnSubscribe<List<ClarifaiOutput<Concept>>>() {
+            return Observable.create(new Observable.OnSubscribe<String>() {
                 @Override
-                public void call(final Subscriber<? super List<ClarifaiOutput<Concept>>> subscriber) {
+                public void call(final Subscriber<? super String> subscriber) {
+                    Model<?> model = client.getModelByID(modelId).executeSync().get();
                     ClarifaiResponse<List<ClarifaiOutput<Concept>>> response = model.asConceptModel() // You can also do client.getModelByID("id") to get custom models
                             .predict()
                             .withInputs(inputImage)
@@ -55,7 +56,19 @@ public class Client {
 
                     // TODO: handle unsuccesful response
                     if (response.isSuccessful()) {
-                        subscriber.onNext(response.get());
+                        List<Concept> concepts = response.get().get(0).data();
+                        String sort = "";
+                        Float maxProbability = 0.6f;
+                        for (Concept c : concepts) {
+                            if (c.value() > maxProbability){
+                                sort = c.name();
+                            }
+                            System.out.println("Concept Id:" + c.id());
+                            System.out.println("Classes:" + c.name());
+                            System.out.println("Probs" + c.value());
+                        }
+                        System.out.println("THIS GARBAGE IS " + sort);
+                        subscriber.onNext(sort);
                         subscriber.onCompleted();
                     }else{
                         System.out.println("-----------  START  ---------------");
@@ -63,8 +76,6 @@ public class Client {
                         System.out.println("CODE: " +response.responseCode());
                         subscriber.onError(new Exception());
                     }
-
-
                 }
             });
 
