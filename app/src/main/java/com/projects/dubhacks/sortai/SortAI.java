a6519a55a2b83c9cc952c4736e8b3a2195d937fe
Soohyun Kim <shkim7534@gmail.com>
2016-10-16 13:02:11 -0700

    pop up work

--- app/src/main/java/com/projects/dubhacks/sortai/SortAI.java
+++ app/src/main/java/com/projects/dubhacks/sortai/SortAI.java
@@ -1,9 +1,13 @@
 package com.projects.dubhacks.sortai;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
+import android.graphics.drawable.Drawable;
 import android.media.ExifInterface;
 import android.media.Image;
 import android.net.Uri;
@@ -15,11 +19,16 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.View;
+import android.view.Window;
 import android.widget.ImageView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
+import android.widget.PopupWindow;
+import android.widget.LinearLayout;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -66,20 +75,20 @@ public class SortAI extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
 //    private File createImageFile() throws IOException {
 //        // Create an image file name
@@ -197,27 +206,58 @@ public class SortAI extends AppCompatActivity {
                             System.out.println(e.getMessage() + " Message -------------------->  HERE");
                         }
 
+
+
                         @Override
                         public void onNext(String sortOutput) {
-                            Toast.makeText(SortAI.this, sortOutput, Toast.LENGTH_LONG).show();
 
-                                    switch(sortOutput) {
+                            Drawable drawable;
+                            Resources r = getApplicationContext().getResources();
+                            switch(sortOutput) {
                                         case "compost":
-                                            setContentView(R.layout.compost_popup);
+                                            drawable = r.getDrawable(R.drawable.compost_popup);
                                             break;
                                         case "recyclable-paper":
-                                            setContentView(R.layout.mixedpaper_popup);
+                                            drawable = r.getDrawable(R.drawable.mixedpaper_popup);
                                             break;
                                         case "containers":
-                                            setContentView(R.layout.recycling_popup);
+                                            drawable = r.getDrawable(R.drawable.recycling_popup);
                                             break;
                                         case "garbage":
-                                            setContentView(R.layout.garbage_popup);
+                                            drawable = r.getDrawable(R.drawable.garbage_popup);
                                             break;
                                         default:
-                                            setContentView(R.layout.error_msg);
+                                            drawable = r.getDrawable(R.drawable.error);
                                             break;
                                     }
+
+                            Dialog dialog = new Dialog(getApplicationContext());
+                            dialog.getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+
+                            dialog.setContentView(getLayoutInflater().inflate(R.layout.activity_sort_ai, null)); //todo
+
+                            ImageView imageView = (ImageView) findViewById(R.id.picture_saving);
+                            imageView.setBackground(drawable);
+
+//                            RelativeLayout relativeLayout = (RelativeLayout) dialog.findViewById(R.id.);
+//                            relativeLayout.setBackground(drawable);
+
+
+//                            PopupWindow popUpWindow = new PopupWindow(getApplicationContext());
+//                            LinearLayout mainLayout = new LinearLayout(getApplicationContext());
+//                            popUpWindow.showAtLocation(mainLayout, Gravity.BOTTOM, 10, 10);
+//                            popUpWindow.update(50, 50, 320, 90);
+//
+//
+//                            View child = getLayoutInflater().inflate(R.layout.compost_popup, null);
+//                            mainLayout.addView(child);
+
+
+//                            Toast.makeText(SortAI.this, sortOutput, Toast.LENGTH_LONG).show();
+
+
+
+
                         }
 
                     });
